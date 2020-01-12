d1c3c15e07b07485154f95c56857f352c61a4f40
MateuszT <mateusz@clearmedia.pl>
2020-01-12 19:15:38 +0100

    Project Setup v2

---
+++ src/app/city/city.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-city',
+  templateUrl: './city.page.html',
+  styleUrls: ['./city.page.scss'],
+})
+export class CityPage implements OnInit {
+
+  data: any;
+  // input_desc_model: string;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe(params => {
+      if (params && params.city_data) {
+        this.data = JSON.parse(params.city_data);
+      }
+
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data = this.router.getCurrentNavigation().extras.state.city_data;
+        // this.input_desc_model = this.data.description;
+      }
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  // ionViewWillLeave() {
+  //   console.log(this.input_desc_model);
+  // }
+
+}
---
+++ src/app/photo/photo.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.page.html',
+  styleUrls: ['./photo.page.scss']
+})
+export class PhotoPage implements OnInit {
+
+  photo: string;
+  
+  cameraOptions: CameraOptions = {
+    quality: 20,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  constructor(private camera: Camera) { }
+
+  takePhoto() {
+    this.camera.getPicture(this.cameraOptions).then((imageData) => {
+      const base64Image = 'data:image/jpeg;base64,' + imageData;
+      this.photo = base64Image;
+    }, (err) => {
+      console.log(err);
+
+    });
+  }
+  ngOnInit() { }
+}
